67de57c296f19ca733bb2d11a9d9113612771c79
AbdulMateenzwl <abdulmateenzwl@gmail.com>
2024-11-16 08:14:06 +0000

    Grafana, temp, loki & project restructure

--- product-service/src/main/java/com/example/product_service/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.product_service.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.product_service.model.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-    
-}
